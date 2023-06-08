b87e668d8b2efaa1620aae91a093cc0ea2be39ea
Bregje Brugman <36854179+BregjeBrugman@users.noreply.github.com>
2023-06-08 10:33:26 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,8 @@ let respecConfig = {
   useLogo: true,
   useLabel: true,
   // title is verplicht! Neem hier de titel van het document op ----------------------
-  title: "Basisregistratie Ondergrond Catalogus Overheidsbesluit bodemverontreiniging, versie 0.5 - 8 juni 2023",
+  title: "Basisregistratie Ondergrond Catalogus Overheidsbesluit bodemverontreiniging",
+  subtitle: "versie 0.5 - 9 juni 2023",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
   specStatus: "wv",                   // Werkversie
   //specStatus: "cv",               // Consultatieversie
