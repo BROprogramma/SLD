df1a303ba0a8ae86029c255db6e501ed548a007b
Bregje Brugman <36854179+BregjeBrugman@users.noreply.github.com>
2023-07-05 08:47:16 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -32,18 +32,6 @@ let respecConfig = {
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
   publishDate: "2023-06-09",
   //custom header lines  
-otherLinks: [
-    {
-      key: "Versie",
-      data: [
-        {
-          value: "0.5",
-          href: "https://docs.geostandaarden.nl/bro/",
-        },
-      ],
-    },
-  ],
-
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
   //previousVersion: "0.0.1",
