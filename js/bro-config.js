29433c89be662e1d1dc89d3ae1b97454d9243150
Bregje Brugman <36854179+BregjeBrugman@users.noreply.github.com>
2023-10-31 22:22:28 +0100

    Update bro-config.js

--- js/bro-config.js
+++ js/bro-config.js
@@ -11,7 +11,7 @@ var organisationConfig = {
     src: "https://docs.geostandaarden.nl/bro/gen/logo_minbzk.png",
     alt: "Ministerie BZK",
     id: "logo_minbzk",
-    width: 449,
+    width: 776,
     height: 163,
     url: "https://www.geobasisregistraties.nl/basisregistraties/ondergrond",
     },
