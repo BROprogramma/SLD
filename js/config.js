fbfb08b2e898741dbca11fa603b4378ab4c32ef7
Arnoud de Boer <a.deboer@geonovum.nl>
2024-02-14 14:52:06 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,8 +5,8 @@ let respecConfig = {
   title: "Basisregistratie Ondergrond Catalogus Overheidsbesluit bodemverontreiniging",
   subtitle: "Versie 0.9",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  specStatus: "wv",                   // Werkversie
-  //specStatus: "cv",               // Consultatieversie
+  //specStatus: "wv",                   // Werkversie
+  specStatus: "cv",               // Consultatieversie
   //specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
@@ -30,14 +30,14 @@ let respecConfig = {
   shortName: "sld",
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  //publishDate: "2024-02-15",
+  publishDate: "2024-02-15",
   //custom header lines  
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
   //previousVersion: "0.0.1",
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
-  previousPublishDate: "2024-02-15",
-  previousMaturity: "cv",
+  //previousPublishDate: "2024-02-15",
+ // previousMaturity: "cv",
   //-- de namen van de Editor(s) / Redacteur(en)---------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   editors:
