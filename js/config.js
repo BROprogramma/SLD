f2a3372d978b4cd9e0a1967e1fea9f9f2b8e076b
Jacob Vos <70584938+jacobvosimpronotion@users.noreply.github.com>
2024-06-12 08:09:26 +0200

    Update config.js
    Waarden aangepast voor 'versie ter vaststelling'

--- js/config.js
+++ js/config.js
@@ -3,11 +3,11 @@ let respecConfig = {
   useLabel: true,
   // title is verplicht! Neem hier de titel van het document op ----------------------
   title: "Basisregistratie Ondergrond Catalogus Overheidsbesluit bodemverontreiniging",
-  subtitle: "Versie 0.9",
+  subtitle: "Versie 1.0",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  specStatus: "wv",                   // Werkversie
+  //specStatus: "wv",                   // Werkversie
   //specStatus: "cv",               // Consultatieversie
-  //specStatus: "vv",               // Versie ter vaststelling
+  specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
@@ -30,7 +30,7 @@ let respecConfig = {
   shortName: "sld",
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  //publishDate: "2024-02-15",
+  publishDate: "2024-06-13",
   //custom header lines  
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
