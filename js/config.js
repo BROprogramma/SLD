b565b8bf212e438bda51576daf4fc05c8631d763
Jacob Vos <70584938+jacobvosimpronotion@users.noreply.github.com>
2024-10-23 08:33:21 +0200

    Update config.js
    Wijziging previousPublishDate en previousMaturity

--- js/config.js
+++ js/config.js
@@ -36,8 +36,8 @@ let respecConfig = {
   //publishVersion: "0.0.2",
   //previousVersion: "0.0.1",
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
-  previousPublishDate: "2024-06-13",
-  previousMaturity: "vv",
+  previousPublishDate: "2024-09-25",
+  previousMaturity: "def",
   //-- de namen van de Editor(s) / Redacteur(en)---------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   editors:
