81f94e70b8322522a75814459037cea84a836057
Jacob Vos <70584938+jacobvosimpronotion@users.noreply.github.com>
2025-10-08 10:10:31 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -33,8 +33,8 @@ let respecConfig = {
   //publishDate: "2024-06-13",
   //custom header lines  
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
-  //publishVersion: "0.0.2",
-  //previousVersion: "0.0.1",
+  publishVersion: " ",
+  previousVersion: " ",
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
   previousPublishDate: "2024-09-25",
   previousMaturity: "def",
