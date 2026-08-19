593f6909939eca4c6af00b7cae441e3d6a0e3b1c
Daoud Urdu <d.urdu@geonovum.nl>
2026-08-19 11:47:30 +0200

    config Aanpassingen tbv release

--- js/config.js
+++ js/config.js
@@ -30,7 +30,7 @@ let respecConfig = {
   shortName: "sld",
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  publishDate: "2026-08-05",
+  publishDate: "2026-08-19",
   //custom header lines  
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   publishVersion: " ",
